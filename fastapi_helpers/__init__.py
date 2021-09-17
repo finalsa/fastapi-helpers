eac88af15d1904b69a71380fb6cd1df4686f3569
Luis Jiménez <luisjimenez6245@hotmail.com>
2021-09-17 00:10:52 -0500

    add dbconfig

--- fastapi_helpers/__init__.py
+++ fastapi_helpers/__init__.py
@@ -2,24 +2,26 @@ from fastapi_helpers.workers import Worker
 from fastapi_helpers.logging import DefaultLogger
 from fastapi_helpers.middlewares import HeadersMiddleware
 from fastapi_helpers.routes import (
-    DefaultModelRouter, 
-    load_data_callback, 
-    Pagination,   
+    DefaultModelRouter,
+    load_data_callback,
+    Pagination,
 )
 from fastapi_helpers.crud import BaseCrud, to_dict
 from fastapi_helpers.security import Encoder
 from fastapi_helpers.settings import DefaultSettings
+from fastapi_helpers.db import DbConfig
 
 __version__ = "0.0.1"
 
 __all__ = [
     "Worker",
     "DefaultLogger",
+    "DbConfig",
     "HeadersMiddleware",
     "DefaultModelRouter",
     "load_data_callback",
     "Pagination",
-    "BaseCrud", 
+    "BaseCrud",
     "to_dict",
     "Encoder",
     "DefaultSettings"
