9a326617d2c166b5041d3ee5966abef98fad25e3
Luis Jiménez <luisjimenez6245@hotmail.com>
2021-09-17 00:52:56 -0500

    fix worker helpers

--- fastapi_helpers/workers/__init__.py
+++ fastapi_helpers/workers/__init__.py
@@ -7,17 +7,20 @@ class Worker:
     db_config: DbConfig
     logger: DefaultLogger
 
-    @classmethod
-    def use_db_connection(cls, func):
+    def __init__(self, db_config:DbConfig, logger: DefaultLogger) -> None:
+        self.db_config = db_config
+        self.logger  = logger
+
+    def use_db_connection(self, func):
         '''Decorator that reports the execution time.'''
 
         async def wrap(*args, **kwargs):
-            await cls.db_config.connect_db(NullPool)
+            await self.db_config.connect_db(NullPool)
             result = None
             try:
                 result = await func(*args, **kwargs)
             except Exception as ex:
-                cls.logger.error(ex)
-            await cls.db_config.disconnect_db()
+                self.logger.error(ex)
+            await self.db_config.disconnect_db()
             return result
         return wrap
