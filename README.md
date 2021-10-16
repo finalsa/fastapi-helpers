64ae0c2bac17ff82325d385262e5b008e35ef501
Luis Jiménez <luisjimenez6245@hotmail.com>
2021-10-16 00:53:53 -0500

    fix issues with seeds

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# fastapi-utils
- 
+# Fastapi Helpers
+
+This pip packages will help you to make your life easier when working with fastapi and ormar.
+
+For installing this package:
+
+```bash
+pip install fastapi-helpers
+```
+
+If you need a default settings for your app, it includes an implemntation BaseSettings of pydantic.
+
+```python
+from fastapi_helpers import DefaultSettings
+from utils import env_path
+from typing import Optional
+
+
+class Settings(DefaultSettings):
+    app_name = "your-app-name"
+    redis_url: Optional[str] = 'redis://localhost:6379'
+    version: Optional[str] = '1.0.0.0'
+    port: Optional[str] = "8000"
+
+
+settings = Settings(env_path)
+```
+
+If you need a logger, it includes an implemntation a colored console, and in prod envs it will log to aws with the help of watchtower.
+
+```python
+from fastapi_helpers import DefaultLogger
+from .config import settings
+
+logger = DefaultLogger("fingeprint", settings)
+```
+
+If you need to connect to a router super fast, the only thing you need to do is to:
+
+```python
+from fastapi_helpers import DbConfig
+from core.config import settings
+from core.logger import logger
+
+db_config = DbConfig(settings, logger)
+```
+
+The are other tools for making the usage of ormar and fastapi even easier.
+
+
+I would realy like to make this tools bigger, but I´m looking for help for documenting this package.
+
+
+Happy codding!
--- fastapi_helpers/db/seeders/reader.py
+++ fastapi_helpers/db/seeders/reader.py
@@ -3,18 +3,24 @@ from json import loads
 from fastapi_helpers.crud import BaseCrud
 
 
+class DbSeeder():
 
-async def read_index(main_path="../seeds/", index_path="index.json"):
-    index = open(path.join(main_path, index_path), "r")
-    index_content = "".join(index.readlines())
-    index.close()
-    classes = loads(index_content)
-    for cls in classes:
-        fil = open(path.join(main_path, cls['name'] + ".json"), "r")
-        fil_content = "".join(fil.readlines())
-        fil.close()
-        objects = loads(fil_content)
-        crud_obj: BaseCrud = eval("crud." + cls['crud'])
-        for obj in objects:
-            await crud_obj.create(obj)
-    return True
+    crud = None
+
+    def __init__(self, crud_module) -> None:
+        self.crud = crud_module
+
+    async def read_index(self, main_path="../seeds/", index_path="index.json"):
+        index = open(path.join(main_path, index_path), "r")
+        index_content = "".join(index.readlines())
+        index.close()
+        classes = loads(index_content)
+        for cls in classes:
+            fil = open(path.join(main_path, cls['name'] + ".json"), "r")
+            fil_content = "".join(fil.readlines())
+            fil.close()
+            objects = loads(fil_content)
+            crud_obj: BaseCrud = eval("self.crud." + cls['crud'])
+            for obj in objects:
+                await crud_obj.create(obj)
+        return True
