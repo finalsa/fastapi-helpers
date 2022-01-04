dae2cbd9506106769e4ac4ce3a91de95ca51c3e5
Luis Jiménez <luisjimenez6245@hotmail.com>
2022-01-03 23:07:54 -0600

    fix version

--- fastapi_helpers/__init__.py
+++ fastapi_helpers/__init__.py
@@ -12,7 +12,7 @@ from fastapi_helpers.security import Encoder
 from fastapi_helpers.settings import DefaultSettings
 from fastapi_helpers.db import DbConfig
 
-__version__ = "0.0.13"
+__version__ = "0.0.14"
 
 __all__ = [
     "Worker",
