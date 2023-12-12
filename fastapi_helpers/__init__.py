93f40c49fdaf4ca44784b8a44a5452588873701a
Luis Jiménez <luisjimenez6245@hotmail.com>
2023-12-11 23:35:44 -0600

    fix issues

--- fastapi_helpers/__init__.py
+++ fastapi_helpers/__init__.py
@@ -34,7 +34,7 @@ from fastapi_helpers.core.settings import DefaultSettings
 from fastapi_helpers.db import DbConfig
 from fastapi_helpers.db.seeders import DbSeeder
 
-__version__ = "0.2.5"
+__version__ = "0.3.0"
 
 __all__ = [
     "get_logger_default_config",
